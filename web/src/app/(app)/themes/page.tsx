cffb0d023a1d22cb24591cc05b1142e6ddba4afb
Akshat Sipany <akshatsipany2106@gmail.com>
2026-09-04 15:04:10 +0530

    refactor: clean up unused imports and table components in themes page

--- web/src/app/(app)/themes/page.tsx
+++ web/src/app/(app)/themes/page.tsx
@@ -9,14 +9,6 @@ import Link from "next/link";
 import { PageHeader } from "@/components/PageHeader";
 import { EmptyState } from "@/components/EmptyState";
 import { DashboardSkeleton } from "@/components/skeletons/LayoutSkeletons";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { useThemes } from "@/lib/hooks";
 import { ThemeCirclePack } from "@/components/themes/ThemeCirclePack";
 import { ThemeInspector } from "@/components/themes/ThemeInspector";
